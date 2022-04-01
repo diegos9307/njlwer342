91c01f5601e86fb43574ce3b60d3ad439b0e84d6
Diego A. Suaza R <diegos9307@gmail.com>
2022-04-01 00:25:43 -0500

    Añadiendo Tareas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      frase: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      frase: e.target.value,
+    });
+    if (e.key === "Enter") {
+      let $capture = document.querySelector(".todo");
+      let $create = document.createElement("li");
+      $create.textContent = `${this.state.frase}`;
+      $capture.appendChild($create);
+      e.target.value = null;
+    }
+  }
   render() {
     return (
       <div className="wrapper">
@@ -13,14 +32,21 @@ class App extends Component {
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              onKeyUp={this.handleChange}
+              type="text"
+              id="new-task"
+              placeholder="Ingresa una tarea y oprime Enter"
+            />
+          </form>
         </div>
       </div>
-    )
+    );
+  }
+  handleSubmit(e) {
+    e.preventDefault();
   }
 }
-
 
 export default App;
